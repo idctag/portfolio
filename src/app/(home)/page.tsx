939d0d380646f70ae118ba8fe4e33d0cd2dd41ca
idctag <idctag@Battulgas-MacBook-Air.local>
2024-10-10 22:37:54 +0800

    backroung effect change

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,7 @@
 import { Link } from "@nextui-org/link";
 import React from "react";
 import { featureLinks } from "../constants";
-import { AuroraBackground } from "../components/ui/aurora-background";
-import { TextGenerateEffect } from "../components/ui/text-generate-effect";
+import { BackgroundBeams } from "../components/ui/background-beams";
 
 interface FeatureLinkProps {
   href: string;
@@ -12,7 +11,12 @@ interface FeatureLinkProps {
 const FeatureLink = (props: FeatureLinkProps) => {
   const { href, name } = props;
   return (
-    <Link className="px-20" href={href}>
+    <Link
+      size="lg"
+      className="hover:bg-neutral-900 px-12 mx-8 rounded-xl"
+      href={href}
+      color="foreground"
+    >
       {name}
     </Link>
   );
@@ -21,9 +25,9 @@ const heading = "Hi I am OJ and this is my ever growing portfolio web page.";
 
 const Home = () => {
   return (
-    <AuroraBackground>
-      <div>
-        <div className="h-[80vh] max-w-screen-lg min-w-5/6 flex flex-col text-center justify-center pb-32">
+    <div className="rounded-md bg-neutral-950 relative antialiased">
+      <div className="h-[100vh] flex flex-col items-center text-center justify-center pb-32">
+        <div className="max-w-screen-lg">
           <h1 className="text-2xl lg:text-5xl">{heading}</h1>
           <h2 className="text-lg lg:text-2xl pt-10">
             I learn and develop different types of web features and list them
@@ -31,7 +35,7 @@ const Home = () => {
           </h2>
         </div>
       </div>
-      <div className="w-full">
+      <div className="w-full relative z-10">
         <div className="flex flex-wrap gap-12 justify-center">
           {featureLinks.map((item) => {
             return (
@@ -40,7 +44,8 @@ const Home = () => {
           })}
         </div>
       </div>
-    </AuroraBackground>
+      <BackgroundBeams />
+    </div>
   );
 };
 
