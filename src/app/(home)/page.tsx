ce13f13ae1a173d641a5dc20a6b4071a967192a9
idctag <idctag@Battulgas-MacBook-Air.local>
2024-10-11 12:45:43 +0800

    commit before auth

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { Link } from "@nextui-org/link";
 import React from "react";
 import { featureLinks } from "../constants";
 import { BackgroundBeams } from "../components/ui/background-beams";
+import { HoverBorderGradient } from "../components/ui/hover-border-gradient";
 
 interface FeatureLinkProps {
   href: string;
@@ -11,14 +13,17 @@ interface FeatureLinkProps {
 const FeatureLink = (props: FeatureLinkProps) => {
   const { href, name } = props;
   return (
-    <Link
-      size="lg"
-      className="hover:bg-neutral-900 px-12 mx-8 rounded-xl"
-      href={href}
-      color="foreground"
-    >
-      {name}
-    </Link>
+    <div>
+      <HoverBorderGradient
+        containerClassName="rounded-full"
+        as="button"
+        className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2 px-12 mx-8"
+      >
+        <Link size="lg" href={href} color="foreground" className="font-bold">
+          {name}
+        </Link>
+      </HoverBorderGradient>
+    </div>
   );
 };
 const heading = "Hi I am OJ and this is my ever growing portfolio web page.";
@@ -35,13 +40,19 @@ const Home = () => {
           </h2>
         </div>
       </div>
-      <div className="w-full relative z-10">
-        <div className="flex flex-wrap gap-12 justify-center">
-          {featureLinks.map((item) => {
-            return (
-              <FeatureLink key={item.name} href={item.href} name={item.name} />
-            );
-          })}
+      <div className="w-full relative z-10 h-[60vh] flex justify-center">
+        <div className="max-w-screen-lg">
+          <div className="flex flex-wrap gap-12 justify-center">
+            {featureLinks.map((item) => {
+              return (
+                <FeatureLink
+                  key={item.name}
+                  href={item.href}
+                  name={item.name}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
       <BackgroundBeams />
